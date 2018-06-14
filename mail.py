7a6fddb74da7f0d9594fea4e098fefc3d719852a
Iaroslav Pavlynskyi <yarbezl@gmail.com>
2018-06-14 22:11:06 +0300

    Added script, that to be able send emails

---
+++ mail.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+
+import smtplib
+
+
+class Gmail(object):
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.server = 'smtp.gmail.com'
+        self.port = 587
+        session = smtplib.SMTP(self.server, self.port)
+        session.ehlo()
+        session.starttls()
+        session.login(self.email, self.password)
+        self.session = session
+
+    def send_message(self, subject, body):
+        ''' This must be removed '''
+        headers = [
+            "From: " + self.email,
+            "Subject: " + subject,
+            "To: " + self.email,
+            "MIME-Version: 1.0",
+            "Content-Type: text/html"]
+        headers = "\r\n".join(headers)
+        self.session.sendmail(
+            self.email,
+            self.email,
+            headers + "\r\n\r\n" + body)
+
+
